638b906676c87a836c6d028d3b75901f93c38cad
Fernando Balieiro <fernandobalieiro@gmail.com>
2020-03-02 16:16:06 +0000

    Initial source code.

---
+++ src/main/java/com/talkdesk/spring/repository/CallerRepository.java
@@ -0,0 +1,34 @@
+package com.talkdesk.spring.repository;
+
+import com.talkdesk.spring.model.Caller;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.IntStream;
+
+public class CallerRepository {
+
+    private Map<String, Caller> callers = new HashMap<>();
+
+    public CallerRepository() {
+        createCallers();
+    }
+
+    public Caller getCallerByPhone(final String phone) {
+        return this.callers.get(phone);
+    }
+
+    private void createCallers() {
+        IntStream.range(0, 10).forEach(i -> {
+                    Caller caller = new Caller();
+                    caller.setFirstName("Caller");
+                    caller.setFirstName("" + i);
+                    caller.setBirthDayDate(new Date());
+                    String phone = "+3519999999" + i;
+                    caller.setPhone(phone);
+                    this.callers.put(phone, caller);
+                }
+        );
+    }
+}
---
+++ src/main/java/com/talkdesk/spring/service/CallerService.java
@@ -0,0 +1,11 @@
+package com.talkdesk.spring.service;
+
+import com.talkdesk.spring.model.Caller;
+
+public class CallerService {
+
+    public Caller getCallerByPhone(final String phone) {
+        // FIXME Invoke method repository here.
+        return null;
+    }
+}
