1be6b7c2529f4d7c5675d5ecaecbd016c66a2c32
Fernando Balieiro <fernandobalieiro@gmail.com>
2020-03-05 15:20:39 +0000

    Added Unit Tests.

---
+++ src/test/java/com/training/spring/test/service/CallerServiceTest.java
@@ -0,0 +1,25 @@
+package com.training.spring.test.service;
+
+import com.training.spring.model.Caller;
+import com.training.spring.service.CallerService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(locations = "classpath:beans-test.xml")
+public class CallerServiceTest {
+
+    @Autowired
+    private CallerService callerService;
+
+    @Test
+    public void test() {
+        Caller caller = callerService.getCallerByPhone("+35199999991");
+        assertEquals("+35199999991", caller.getPhone());
+    }
+}
