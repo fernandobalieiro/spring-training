9554f2a1a531422d0aadb14ab76f6e90c1c94616
Fernando Balieiro <fernandobalieiro@gmail.com>
2024-07-01 17:46:49 +0100

    Upgraded Spring Boot version.

--- src/test/java/com/training/spring/test/service/CallLogServiceTest.java
+++ src/test/java/com/training/spring/test/service/CallLogServiceTest.java
@@ -1,25 +1,24 @@
 package com.training.spring.test.service;
 
-import com.training.spring.AppConfig;
 import com.training.spring.model.CallLog;
 import com.training.spring.service.CallLogService;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@RunWith(SpringRunner.class)
-@ContextConfiguration(classes = AppConfig.class)
-public class CallLogServiceTest {
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "classpath:beans-test.xml")
+class CallLogServiceTest {
 
     @Autowired
     private CallLogService callLogService;
 
     @Test
-    public void test() {
+    void test() {
         CallLog callLog = callLogService.getCallLogByPhone("+35199999991");
         assertEquals("+35199999991", callLog.getSource());
     }
